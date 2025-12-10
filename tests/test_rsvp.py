f4493ed5d9474619083ba6ff9d61b22abcbd3a28
ujjalpoudel75 <ujjalpoudel75@gmail.com>
2025-12-10 11:49:29 +0900

    something changed

--- tests/test_rsvp.py
+++ tests/test_rsvp.py
@@ -1,3 +1,7 @@
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 import pytest
 from src.rsvp import dedupe_emails_case_preserve_order, first_with_domain, domain_counts
 
